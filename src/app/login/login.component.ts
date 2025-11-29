ab039fb34ca88fb2203ffdc85de765284c7d8d9d
Arthur Martins Alves <oarthurma@gmail.com>
2025-11-29 10:58:51 -0300

    antes de iniciar as validações dinâmicas no login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],     // importa FormsModule 
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  login() {
+    console.log('Email:', this.email);
+    console.log('Senha:', this.password);
+  }
+}
+
+  
