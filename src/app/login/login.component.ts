19dd76e5cf28b4bcd0e093d2315245c0a209866e
Arthur Martins Alves <oarthurma@gmail.com>
2025-11-30 11:12:37 -0300

    antes de criar o home

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],     // importa FormsModule 
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],     
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
